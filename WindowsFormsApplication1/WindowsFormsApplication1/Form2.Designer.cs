aaa439438265ae7ef66e09b85c5c05d5a962b6e7
QAsMD <ginromdam@gmail.com>
2014-04-05 00:05:18 +0300

    работает

--- WindowsFormsApplication1/WindowsFormsApplication1/Form2.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form2.Designer.cs
@@ -40,16 +40,19 @@
             this.webBrowser1.Location = new System.Drawing.Point(0, 0);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(284, 262);
+            this.webBrowser1.Size = new System.Drawing.Size(503, 389);
             this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 367);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(503, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -63,11 +66,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(503, 389);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.webBrowser1);
             this.Name = "Form2";
-            this.Text = "Form2";
+            this.Text = "Авторизация";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
--- WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
@@ -47,5 +47,7 @@ namespace WindowsFormsApplication1
             }
             catch { }
         }
+
+       
     }
 }
