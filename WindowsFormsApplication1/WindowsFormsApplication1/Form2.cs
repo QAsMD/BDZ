3dc0a0d549a0e4c236c7aa739f9a5d951de0c52e
QAsMD <ginromdam@gmail.com>
2014-04-04 23:54:16 +0300

    получили токены

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://oauth.vk.com/authorize?client_id=4221474&scope=messages,offline&redirect_uri=https://oauth.vk.com/blank.html&display=popup&v=5.15&response_type=token ");
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Загрузка";
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Загружено";
+
+            try
+            {
+
+                string url = webBrowser1.Url.ToString();
+                string l = url.Split('#')[1];
+                if (l[0] == 'a')
+                {
+                    Settings1.Default.token = l.Split('&')[0].Split('=')[1];
+                    Settings1.Default.id = l.Split('=')[3];
+                    Settings1.Default.auth = true;
+                    MessageBox.Show(Settings1.Default.token + " " + Settings1.Default.id);
+                    this.Close();
+                }
+            }
+            catch { }
+        }
+    }
+}
