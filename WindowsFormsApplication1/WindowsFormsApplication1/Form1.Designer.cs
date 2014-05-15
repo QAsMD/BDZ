ded191c5f7fd433bdea26c9eaff356dcd3e31626
Grigory <grigoryvydrin@gmail.com>
2014-05-15 22:52:14 +0400

    Изменен дизайн главной формой

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,11 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.connectAPIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.btn_Profile = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btn_Friends = new System.Windows.Forms.Button();
             this.btn_Messages = new System.Windows.Forms.Button();
             this.pictureBoxProfile = new System.Windows.Forms.PictureBox();
@@ -41,63 +37,17 @@
             this.labFriends = new System.Windows.Forms.Label();
             this.labTextLastSeen = new System.Windows.Forms.Label();
             this.labTextStatus = new System.Windows.Forms.Label();
-            this.menuStrip1.SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).BeginInit();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.menuToolStripMenuItem,
-            this.exitToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(540, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // menuToolStripMenuItem
-            // 
-            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.connectAPIToolStripMenuItem});
-            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
-            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
-            this.menuToolStripMenuItem.Text = "Menu";
-            // 
-            // connectAPIToolStripMenuItem
-            // 
-            this.connectAPIToolStripMenuItem.Name = "connectAPIToolStripMenuItem";
-            this.connectAPIToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
-            this.connectAPIToolStripMenuItem.Text = "Connect API";
-            this.connectAPIToolStripMenuItem.Click += new System.EventHandler(this.connectAPIToolStripMenuItem_Click);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
-            // 
-            // btn_Profile
-            // 
-            this.btn_Profile.Enabled = false;
-            this.btn_Profile.Location = new System.Drawing.Point(9, 25);
-            this.btn_Profile.Margin = new System.Windows.Forms.Padding(2);
-            this.btn_Profile.Name = "btn_Profile";
-            this.btn_Profile.Size = new System.Drawing.Size(143, 39);
-            this.btn_Profile.TabIndex = 1;
-            this.btn_Profile.Text = "Profile";
-            this.btn_Profile.UseVisualStyleBackColor = true;
-            this.btn_Profile.Click += new System.EventHandler(this.btn_Profile_Click);
             // 
             // btn_Friends
             // 
             this.btn_Friends.Enabled = false;
-            this.btn_Friends.Location = new System.Drawing.Point(199, 25);
+            this.btn_Friends.Location = new System.Drawing.Point(234, 131);
             this.btn_Friends.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Friends.Name = "btn_Friends";
-            this.btn_Friends.Size = new System.Drawing.Size(143, 39);
+            this.btn_Friends.Size = new System.Drawing.Size(240, 39);
             this.btn_Friends.TabIndex = 2;
             this.btn_Friends.Text = "Music";
             this.btn_Friends.UseVisualStyleBackColor = true;
@@ -105,10 +55,10 @@
             // btn_Messages
             // 
             this.btn_Messages.Enabled = false;
-            this.btn_Messages.Location = new System.Drawing.Point(388, 25);
+            this.btn_Messages.Location = new System.Drawing.Point(234, 174);
             this.btn_Messages.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Messages.Name = "btn_Messages";
-            this.btn_Messages.Size = new System.Drawing.Size(143, 39);
+            this.btn_Messages.Size = new System.Drawing.Size(240, 39);
             this.btn_Messages.TabIndex = 3;
             this.btn_Messages.Text = "Messages";
             this.btn_Messages.UseVisualStyleBackColor = true;
@@ -117,10 +67,10 @@
             // pictureBoxProfile
             // 
             this.pictureBoxProfile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBoxProfile.Location = new System.Drawing.Point(11, 68);
+            this.pictureBoxProfile.Location = new System.Drawing.Point(11, 11);
             this.pictureBoxProfile.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBoxProfile.Name = "pictureBoxProfile";
-            this.pictureBoxProfile.Size = new System.Drawing.Size(219, 295);
+            this.pictureBoxProfile.Size = new System.Drawing.Size(219, 254);
             this.pictureBoxProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxProfile.TabIndex = 4;
             this.pictureBoxProfile.TabStop = false;
@@ -128,17 +78,18 @@
             // labName
             // 
             this.labName.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labName.Location = new System.Drawing.Point(234, 68);
+            this.labName.Location = new System.Drawing.Point(234, 11);
             this.labName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labName.Name = "labName";
-            this.labName.Size = new System.Drawing.Size(212, 39);
+            this.labName.Size = new System.Drawing.Size(240, 58);
             this.labName.TabIndex = 5;
             this.labName.Text = "name_user";
+            this.labName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // labStatus
             // 
             this.labStatus.AutoSize = true;
-            this.labStatus.Location = new System.Drawing.Point(235, 107);
+            this.labStatus.Location = new System.Drawing.Point(236, 69);
             this.labStatus.Name = "labStatus";
             this.labStatus.Size = new System.Drawing.Size(43, 13);
             this.labStatus.TabIndex = 6;
@@ -147,7 +98,7 @@
             // labFriends
             // 
             this.labFriends.AutoSize = true;
-            this.labFriends.Location = new System.Drawing.Point(235, 134);
+            this.labFriends.Location = new System.Drawing.Point(236, 98);
             this.labFriends.Name = "labFriends";
             this.labFriends.Size = new System.Drawing.Size(61, 13);
             this.labFriends.TabIndex = 7;
@@ -156,7 +107,7 @@
             // labTextLastSeen
             // 
             this.labTextLastSeen.AutoSize = true;
-            this.labTextLastSeen.Location = new System.Drawing.Point(296, 134);
+            this.labTextLastSeen.Location = new System.Drawing.Point(297, 98);
             this.labTextLastSeen.Name = "labTextLastSeen";
             this.labTextLastSeen.Size = new System.Drawing.Size(0, 13);
             this.labTextLastSeen.TabIndex = 8;
@@ -164,16 +115,27 @@
             // labTextStatus
             // 
             this.labTextStatus.AutoSize = true;
-            this.labTextStatus.Location = new System.Drawing.Point(296, 107);
+            this.labTextStatus.Location = new System.Drawing.Point(285, 69);
             this.labTextStatus.Name = "labTextStatus";
             this.labTextStatus.Size = new System.Drawing.Size(0, 13);
             this.labTextStatus.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 39);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Connect API";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 374);
+            this.ClientSize = new System.Drawing.Size(485, 278);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.labTextStatus);
             this.Controls.Add(this.labTextLastSeen);
             this.Controls.Add(this.labFriends);
@@ -182,14 +144,9 @@
             this.Controls.Add(this.pictureBoxProfile);
             this.Controls.Add(this.btn_Messages);
             this.Controls.Add(this.btn_Friends);
-            this.Controls.Add(this.btn_Profile);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Encryption Message";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -198,11 +155,6 @@
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem connectAPIToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
-        private System.Windows.Forms.Button btn_Profile;
         private System.Windows.Forms.Button btn_Friends;
         private System.Windows.Forms.Button btn_Messages;
         private System.Windows.Forms.PictureBox pictureBoxProfile;
@@ -211,6 +163,7 @@
         private System.Windows.Forms.Label labFriends;
         private System.Windows.Forms.Label labTextLastSeen;
         private System.Windows.Forms.Label labTextStatus;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -18,9 +18,6 @@ namespace WindowsFormsApplication1
         ImageList list_image = new ImageList();
         public Form1()
         {
-            
-            
-
             InitializeComponent();
         }
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -33,44 +30,54 @@ namespace WindowsFormsApplication1
             {
                 new Form2().Show();
             }
-            
-            btn_Friends.Enabled = true;
-            btn_Messages.Enabled = true;
-            btn_Profile.Enabled = true;
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn_Profile_Click(object sender, EventArgs e)
-        {
-            string name_user = String.Empty;
-            string image_user = String.Empty;
-
-            var result = lib_vk.users_get("21881340", "photo_max_orig, status, last_seen, counters");
-            string url_photo = result["photo_max_orig"];
-            try
-            {
-                var status = result["status_audio"];
-                labStatus.Text = "AudioPlay: ";
-                labTextStatus.Text = status["artist"] + " - " + status["title"];
-            }
-            catch
-            {
-                labStatus.Text = "Status: ";
-                labTextStatus.Text = result["status"];
-            }
-            name_user = result["first_name"] + " " + result["last_name"];
-            labTextLastSeen.Text = lib_vk.ConvertFromUnixTimestamp((double)result["last_seen"]["time"]);
-            pictureBoxProfile.Load(url_photo);
-            labName.Text = name_user;
         }
 
         private void btn_Messages_Click(object sender, EventArgs e)
         {
             new Messages().Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Connect API")
+            {
+                if (!Settings1.Default.auth)
+                {
+                    new Form2().Show();
+                }
+
+                btn_Friends.Enabled = true;
+                btn_Messages.Enabled = true;
+
+                string name_user = String.Empty;
+                string image_user = String.Empty;
+
+                var result = lib_vk.users_get("21881340", "photo_max_orig, status, last_seen, counters");
+                string url_photo = result["photo_max_orig"];
+                try
+                {
+                    var status = result["status_audio"];
+                    labStatus.Text = "AudioPlay: ";
+                    labTextStatus.Text = status["artist"] + " - " + status["title"];
+                }
+                catch
+                {
+                    labStatus.Text = "Status: ";
+                    labTextStatus.Text = result["status"];
+                }
+                name_user = result["first_name"] + " " + result["last_name"];
+                labTextLastSeen.Text = lib_vk.ConvertFromUnixTimestamp((double)result["last_seen"]["time"]);
+                pictureBoxProfile.Load(url_photo);
+                labName.Text = name_user;
+
+                btn_Friends.Enabled = true;
+                btn_Messages.Enabled = true;
+                button1.Text = "Exit";
+            }
+            else if (button1.Text == "Exit")
+            {
+                Application.Exit();
+            }
         }
     }
 }
