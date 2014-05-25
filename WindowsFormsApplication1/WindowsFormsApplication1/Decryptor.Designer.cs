4c6c7c08415a492ebe0d0b9d7b33b01ea4c3a184
Grigory <grigoryvydrin@gmail.com>
2014-05-25 17:59:36 +0400

    Добавлена форма Decryptor

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Decryptor.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Decryptor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Decryptor));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IconEncrypt = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NameBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 374);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Url_Content";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Decrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 371);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(410, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(87, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(499, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите ссылку с контентом для расшифровки";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // IconEncrypt
+            // 
+            this.IconEncrypt.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.IconEncrypt.Icon = ((System.Drawing.Icon)(resources.GetObject("IconEncrypt.Icon")));
+            this.IconEncrypt.Text = "Status Decrypt";
+            this.IconEncrypt.Visible = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 483);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(677, 25);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(154, 20);
+            this.toolStripStatusLabel1.Text = "Состояние загрузки: ";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 19);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 402);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Name_File";
+            // 
+            // NameBox2
+            // 
+            this.NameBox2.Location = new System.Drawing.Point(176, 399);
+            this.NameBox2.Name = "NameBox2";
+            this.NameBox2.Size = new System.Drawing.Size(410, 22);
+            this.NameBox2.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(329, 296);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(341, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(306, 296);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Message: ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(653, 323);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Decrypted Data";
+            // 
+            // Decryptor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 508);
+            this.Controls.Add(this.NameBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Decryptor";
+            this.Text = "Decryptor";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon IconEncrypt;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox NameBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Decryptor.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Decryptor : Form
+    {
+        public Decryptor()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && NameBox2.Text!= "")
+            {
+                label4.Text = "Message: ";
+                pictureBox1.Load(textBox1.Text);
+                download_content(NameBox2.Text, textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("Заполните все поля формы!");
+            }
+        }
+
+        private void download_content(string name_file, string url, string type_content=".jpg")
+        {
+            name_file += type_content;
+            // Делаем загрузку с помощью System.Net.WebClient.
+            var webClient = new WebClient();
+            var beforeProgress = 0; // счетчик прогресса
+
+            // подписываемся на событие изменения прогресса, чтобы выводить на экран
+            webClient.DownloadProgressChanged += (s, e) =>
+            {
+                // ну и выводим соответственно через каждые 10%
+                if (e.ProgressPercentage - 10 > beforeProgress)
+                {
+                    beforeProgress += 10;
+                    toolStripProgressBar1.Value = beforeProgress;
+                }
+            };
+
+            // подписываемся на событие окончания загрузки, чтобы знать когда загрузка закончится
+            webClient.DownloadFileCompleted += (s, e) =>
+            {
+                toolStripProgressBar1.Value += 10;
+                IconEncrypt.BalloonTipTitle = "Status decrypt:";
+                IconEncrypt.BalloonTipText = "File downloaded: Done";
+                IconEncrypt.ShowBalloonTip(1000);
+                string encrypt_message = get_encrypt_message(name_file, name_file);
+                IconEncrypt.BalloonTipText = "Data encrypt get: Done.";
+                IconEncrypt.ShowBalloonTip(1000);
+                var result = ClassSecurity.Decrypt(encrypt_message, name_file.Substring(name_file.LastIndexOf("_") + 1));
+                IconEncrypt.BalloonTipText = "Data decrypt get: Done.";
+                IconEncrypt.ShowBalloonTip(1000);
+                label4.Text += result;
+            };
+
+            // запускаем загрузку асинхронно.
+            webClient.DownloadFileAsync(new Uri(url), name_file);
+        }
+
+        private string get_encrypt_message(string name_file, string path_file)
+        {
+            string key = name_file.Substring(name_file.LastIndexOf("_") + 1);
+            string str_size;
+            byte[] message;
+
+            using (StreamReader sr = new StreamReader(path_file))
+            {
+                var result = sr.ReadToEnd();
+                str_size = result.Substring(result.LastIndexOf("_") + 1);
+                sr.Close();
+            }
+
+            using (FileStream fs = File.OpenRead(path_file))
+            {
+                int size_read = (int)fs.Length - Convert.ToInt32(str_size) - str_size.Length - 1;
+                message = new byte[size_read];
+                fs.Seek(Convert.ToInt32(str_size), SeekOrigin.Current);
+                fs.Read(message, 0, size_read);
+
+            }
+            return Encoding.Default.GetString(message);
+        }
+    }
+}
