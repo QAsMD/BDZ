39fef353fa98e3bee40baf8f0851c81afea0812d
Grigory <grigoryvydrin@gmail.com>
2014-05-25 18:00:07 +0400

    Fix main form and form messages

--- WindowsFormsApplication1/WindowsFormsApplication1/Messages.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Messages.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Messages));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ListChat = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.butSend = new System.Windows.Forms.Button();
             this.butAttachment = new System.Windows.Forms.Button();
@@ -42,7 +43,6 @@
             this.labNameFriend = new System.Windows.Forms.Label();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.ListChat = new System.Windows.Forms.RichTextBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAttach)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -55,28 +55,44 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.butSend);
             this.groupBox1.Controls.Add(this.butAttachment);
-            this.groupBox1.Location = new System.Drawing.Point(4, 6);
+            this.groupBox1.Location = new System.Drawing.Point(5, 7);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(389, 345);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(519, 425);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Chat User";
             this.groupBox1.UseCompatibleTextRendering = true;
             // 
+            // ListChat
+            // 
+            this.ListChat.Location = new System.Drawing.Point(8, 23);
+            this.ListChat.Margin = new System.Windows.Forms.Padding(4);
+            this.ListChat.Name = "ListChat";
+            this.ListChat.ReadOnly = true;
+            this.ListChat.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.ListChat.Size = new System.Drawing.Size(501, 219);
+            this.ListChat.TabIndex = 3;
+            this.ListChat.Text = "";
+            this.ListChat.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ListChat_MouseDoubleClick);
+            // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(8, 201);
+            this.label1.Location = new System.Drawing.Point(11, 247);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(160, 23);
+            this.label1.Size = new System.Drawing.Size(213, 28);
             this.label1.TabIndex = 1;
             this.label1.Text = "Message Input:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // butSend
             // 
-            this.butSend.Location = new System.Drawing.Point(265, 283);
+            this.butSend.Location = new System.Drawing.Point(353, 348);
+            this.butSend.Margin = new System.Windows.Forms.Padding(4);
             this.butSend.Name = "butSend";
-            this.butSend.Size = new System.Drawing.Size(116, 54);
+            this.butSend.Size = new System.Drawing.Size(155, 66);
             this.butSend.TabIndex = 1;
             this.butSend.Text = "Send";
             this.butSend.UseVisualStyleBackColor = true;
@@ -84,9 +100,10 @@
             // 
             // butAttachment
             // 
-            this.butAttachment.Location = new System.Drawing.Point(265, 227);
+            this.butAttachment.Location = new System.Drawing.Point(353, 279);
+            this.butAttachment.Margin = new System.Windows.Forms.Padding(4);
             this.butAttachment.Name = "butAttachment";
-            this.butAttachment.Size = new System.Drawing.Size(116, 50);
+            this.butAttachment.Size = new System.Drawing.Size(155, 62);
             this.butAttachment.TabIndex = 2;
             this.butAttachment.Text = "Attach";
             this.butAttachment.UseVisualStyleBackColor = true;
@@ -94,17 +111,19 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 233);
+            this.richTextBox1.Location = new System.Drawing.Point(16, 287);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(251, 110);
+            this.richTextBox1.Size = new System.Drawing.Size(333, 134);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // pictureBoxAttach
             // 
-            this.pictureBoxAttach.Location = new System.Drawing.Point(6, 19);
+            this.pictureBoxAttach.Location = new System.Drawing.Point(8, 23);
+            this.pictureBoxAttach.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBoxAttach.Name = "pictureBoxAttach";
-            this.pictureBoxAttach.Size = new System.Drawing.Size(176, 173);
+            this.pictureBoxAttach.Size = new System.Drawing.Size(235, 213);
             this.pictureBoxAttach.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxAttach.TabIndex = 3;
             this.pictureBoxAttach.TabStop = false;
@@ -112,9 +131,11 @@
             // listBoxFriends
             // 
             this.listBoxFriends.FormattingEnabled = true;
-            this.listBoxFriends.Location = new System.Drawing.Point(6, 210);
+            this.listBoxFriends.ItemHeight = 16;
+            this.listBoxFriends.Location = new System.Drawing.Point(8, 258);
+            this.listBoxFriends.Margin = new System.Windows.Forms.Padding(4);
             this.listBoxFriends.Name = "listBoxFriends";
-            this.listBoxFriends.Size = new System.Drawing.Size(385, 121);
+            this.listBoxFriends.Size = new System.Drawing.Size(512, 148);
             this.listBoxFriends.TabIndex = 4;
             this.listBoxFriends.SelectedIndexChanged += new System.EventHandler(this.listBoxFriends_SelectedIndexChanged);
             // 
@@ -130,9 +151,11 @@
             this.groupBox2.Controls.Add(this.labNameFriend);
             this.groupBox2.Controls.Add(this.listBoxFriends);
             this.groupBox2.Controls.Add(this.pictureBoxAttach);
-            this.groupBox2.Location = new System.Drawing.Point(399, 6);
+            this.groupBox2.Location = new System.Drawing.Point(532, 7);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(397, 337);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(529, 415);
             this.groupBox2.TabIndex = 7;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Friends";
@@ -140,18 +163,20 @@
             // labStatusUser
             // 
             this.labStatusUser.AutoSize = true;
-            this.labStatusUser.Location = new System.Drawing.Point(274, 87);
+            this.labStatusUser.Location = new System.Drawing.Point(365, 126);
+            this.labStatusUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labStatusUser.Name = "labStatusUser";
-            this.labStatusUser.Size = new System.Drawing.Size(35, 13);
+            this.labStatusUser.Size = new System.Drawing.Size(46, 17);
             this.labStatusUser.TabIndex = 6;
             this.labStatusUser.Text = "label2";
             // 
             // labNameFriend
             // 
             this.labNameFriend.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labNameFriend.Location = new System.Drawing.Point(188, 37);
+            this.labNameFriend.Location = new System.Drawing.Point(251, 46);
+            this.labNameFriend.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labNameFriend.Name = "labNameFriend";
-            this.labNameFriend.Size = new System.Drawing.Size(203, 50);
+            this.labNameFriend.Size = new System.Drawing.Size(271, 80);
             this.labNameFriend.TabIndex = 5;
             this.labNameFriend.Text = "label2";
             this.labNameFriend.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -160,38 +185,30 @@
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 354);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 438);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(803, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1071, 25);
             this.statusStrip1.TabIndex = 8;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(187, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(239, 20);
             this.toolStripStatusLabel1.Text = "Контент к отправке не добавлен.";
-            // 
-            // ListChat
-            // 
-            this.ListChat.Location = new System.Drawing.Point(6, 19);
-            this.ListChat.Name = "ListChat";
-            this.ListChat.ReadOnly = true;
-            this.ListChat.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.ListChat.Size = new System.Drawing.Size(377, 179);
-            this.ListChat.TabIndex = 3;
-            this.ListChat.Text = "";
             // 
             // Messages
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(803, 376);
+            this.ClientSize = new System.Drawing.Size(1071, 463);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Messages";
             this.Text = "Messages VK";
             this.groupBox1.ResumeLayout(false);
